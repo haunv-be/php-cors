6772bd35ddefe854c3ad40e95c1871482d60b6f3
haunv-be <haunv.be@gmail.com>
2025-01-26 14:21:15 +0700

    feat: add `HttpResponse` class

---
+++ src/Cors/HttpResponse.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Enlightener\Cors;
+
+use Enlightener\Cors\Utils;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+
+class HttpResponse
+{
+    /**
+     * Headers attached the "preflight" response sent by the server side
+     * before receiving the "actual" request. These headers force sent such as
+     * "allow: origin, headers, methods" and optional "max-age" if any.
+     */
+    public const VARY = 'Vary';
+    public const ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin';
+    public const ACCESS_CONTROL_ALLOW_HEADERS = 'Access-Control-Allow-Headers';
+    public const ACCESS_CONTROL_ALLOW_METHODS = 'Access-Control-Allow-Methods';
+    public const ACCESS_CONTROL_ALLOW_CREDENTIALS = 'Access-Control-Allow-Credentials';
+    public const ACCESS_CONTROL_EXPOSE_HEADERS = 'Access-Control-Expose-Headers';
+    public const ACCESS_CONTROL_MAX_AGE = 'Access-Control-Max-Age';
+
+    /**
+     * The response instance.
+     *
+     * @var Response|JsonResponse|RedirectResponse
+     */
+    protected $response;
+
+    /**
+     * Create a new response decorator instance.
+     */
+    public function __construct(Response|JsonResponse|RedirectResponse $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Get the response instance.
+     */
+    public function instance(): Response|JsonResponse|RedirectResponse
+    {
+        return $this->response;
+    }
+
+    /**
+     * Get the response header value with the given key.
+     */
+    public function header(string $key): ?string
+    {
+        return $this->response->headers->get($key);
+    }
+
+    /**
+     * Set the response header value with the given arguments.
+     */
+    public function setHeader(string $key, string|array|null $values): void
+    {
+        $this->response->headers->set($key, $values);
+    }
+
+    /**
+     * Determine if the response header exists.
+     */
+    public function hasHeader(string $key): bool
+    {
+        return $this->response->headers->has($key);
+    }
+
+    /**
+     * Determine if the response has the "vary" header.
+     */
+    protected function hasVaryHeader(): bool
+    {
+        return $this->hasHeader(self::VARY);
+    }
+
+    /**
+     * Determine if the response is without the "vary" header.
+     */
+    protected function withoutVaryHeader(): bool
+    {
+        return ! $this->hasVaryHeader();
+    }
+
+    /**
+     * Get the response "vary" header value.
+     */
+    protected function varyHeader(): ?string
+    {
+        return $this->header(self::VARY);
+    }
+
+    /**
+     * Set the response "vary" header with the given value.
+     */
+    public function setVaryHeader(string $value): void
+    {
+        if ($this->withoutVaryHeader()) {
+            $this->setHeader(self::VARY, $value);
+        } elseif (Utils::strNotContains($this->varyHeader(), $value)) {
+            $this->setHeader(self::VARY, "{$this->varyHeader()}, {$value}");
+        }
+    }
+
+    /**
+     * Dynamically retrieve attributes on the response decorator instance.
+     */
+    public function __get(string $key): mixed
+    {
+        if ($key === 'headers') {
+            return $this->response->{$key};
+        }
+    }
+
+    /**
+     * Dynamically handle calls into the response decorator instance.
+     */
+    public function __call(string $method, array $parameters): mixed
+    {
+        return $this->response->{$method}(...$parameters);
+    }
+}
