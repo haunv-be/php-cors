ffd83ee7b9886aa99cfc4b0d218029a26568b478
haunv-be <haunv.be@gmail.com>
2025-02-01 16:27:28 +0700

    refactor: update code for `CorsDispatcher` class

--- src/Cors/CorsDispatcher.php
+++ src/Cors/CorsDispatcher.php
@@ -3,6 +3,7 @@
 namespace Enlightener\Cors;
 
 use Closure;
+use Enlightener\Cors\Utils;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Enlightener\Cors\CorsManager;
@@ -34,19 +35,60 @@ class CorsDispatcher
     {
         $middleware = new Middleware;
 
-        // First, we'll determine if the request "origin" header value
-        // matches the "origin" value in the collection.
-        // If exactly, we get the cors service instance in the collection and
-        // setting it into middleware.
-        $origin = $request->headers->get(HttpRequest::ORIGIN) ?? '*';
+        $collection = $this->manager->collection();
 
-        if (! is_null($instance = $this->manager->collection()->get($origin))) {
-            $middleware->setCorsService($instance);
+        // If the incoming request is sent by the same host, or collection is empty
+        // then we'll create a new cors service with the default options.
+        if ($this->isSameHost($request) || $collection->isEmpty()) {
+            $instance = $this->manager->origins('*')->current();
+        }
+
+        // If the collection exists the wildcard value then
+        // we get the cors service instance of that value
+        // and ignore all remaining values if any.
+        elseif ($collection->has('*')) {
+            $instance = $collection->get('*');
+        }
+
+        // If the incoming request exists the "origin" header value
+        // and matches the "origin" value in the collection.
+        // Then we get the cors service instance of this value.
+        elseif (! is_null($origin = $request->headers->get(HttpRequest::ORIGIN)) &&
+                $collection->has($origin)) {
+            $instance = $collection->get($origin);
+        }
+
+        // If the incoming request does not pass any of the conditions above
+        // then this request is not allowed from our server. We'll create a new cors service
+        // with the custom origin value to automatically throw exceptions in configuration steps.
+        else {
+            $instance = $this->manager->origins(
+                'https://'.Utils::strRandom(10).'.com'
+            )->current();
         }
         
-        // If the request "origin" header value no has on this request or
-        // you have not yet set any values into the collection,
-        // then the default options will be set into middleware.
-        return $middleware->handle($request, $next);
+        return $middleware->setCorsService($instance)->handle($request, $next);
+    }
+
+    /**
+     * Determine if the incoming request is the same host.
+     */
+    protected function isSameHost(Request $request): bool
+    {
+        $origin = $request->headers->get(HttpRequest::ORIGIN);
+
+        $schemeAndHttpHost = $request->getSchemeAndHttpHost();
+
+        return is_null($origin) ||
+               $origin === $schemeAndHttpHost;
+               $origin === rtrim(env('APP_URL') ?? '', '/');
+    }
+
+    /**
+     * Determine if the incoming request is not the same host.
+     */
+    protected function isNotSameHost(Request $request): bool
+    {
+        return ! $this->isSameHost($request);
     }
 }
